b92e7990e791c2058fa1dce4ad3b8e08b6a52e6f
Connor-Karpyszyn <43984978+Connor-Karpyszyn@users.noreply.github.com>
2020-09-23 00:05:06 +0100

    File Upload

---
+++ BookListMVC/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListMVC.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BookListMVC
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+		services.AddDbContext<ApplicationDbContext> (options=>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); //Code needed to connect with db sql
+		services.AddControllersWithViews().AddRazorRuntimeCompilation(); // updates Page on save
+			services.AddRazorPages();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default", // Defualt is Index, so if nothing is found index is called.
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapRazorPages(); 
+			});
+		}
+	}
+}
